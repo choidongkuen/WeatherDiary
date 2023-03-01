d3ca2d9b1cefd055e563eab65df6612a12a576d6
choidongkuen <danaver12@daum.net>
2023-03-02 02:26:09 +0900

    refactor : createDiary 수정 (RequestBody 에 String 보내는 경우)

--- src/main/java/com/example/weatherdiary/controller/DiaryController.java
+++ src/main/java/com/example/weatherdiary/controller/DiaryController.java
@@ -5,16 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/diary")
 public class DiaryController {
 
     private final DiaryService diaryService;
@@ -22,9 +22,9 @@ public class DiaryController {
     @PostMapping()
     public ResponseEntity createDiary(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-            @RequestBody String text
-    ) {
-        diaryService.createDiary(date,text);
+            @RequestBody Map<String, String> text
+    ) throws IOException {
+        diaryService.createDiary(date,text.get("text"));
         return ResponseEntity.ok().build();
     }
 }
