f340b925b97baccc31fe94c23945ced4aeddd996
choidongkuen <danaver12@daum.net>
2023-03-02 00:25:02 +0900

    feat : createDiary test controller 생성

--- src/test/java/com/example/weatherdiary/controller/DiaryControllerTest.java
+++ src/test/java/com/example/weatherdiary/controller/DiaryControllerTest.java
@@ -6,8 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(DiaryController.class)
 class DiaryControllerTest {
@@ -16,13 +25,25 @@ class DiaryControllerTest {
     private DiaryService diaryService;
 
     @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
     private ObjectMapper objectMapper;
 
-
     @Test
-    void createDiary() {
+    void createDiary() throws Exception {
         // given
+        given(diaryService.createDiary(any(), anyString()))
+                .willReturn("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=aa9b8a3df6d399327837db0c3ea7dae5");
+
+        LocalDate date = LocalDate.of(2023, 3, 1);
         // when
+        mockMvc.perform(post("/diary")
+                       .param("date", String.valueOf(date))
+                       .contentType(MediaType.APPLICATION_JSON)
+                       .content(objectMapper.writeValueAsString(new String("안녕하세요"))))
+               .andExpect(status().isOk())
+               .andDo(print());
         // then
     }
 
