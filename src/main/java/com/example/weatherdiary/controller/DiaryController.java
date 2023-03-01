ef5029ec9e25c0b7e746b41d2f547e69a20d1be3
choidongkuen <danaver12@daum.net>
2023-03-02 04:21:10 +0900

    feat : 일기 수정 컨트롤러 구현

--- src/main/java/com/example/weatherdiary/controller/DiaryController.java
+++ src/main/java/com/example/weatherdiary/controller/DiaryController.java
@@ -1,8 +1,11 @@
 package com.example.weatherdiary.controller;
 
 import com.example.weatherdiary.domain.entity.Diary;
+import com.example.weatherdiary.dto.CreateDiaryRequestDto;
 import com.example.weatherdiary.dto.DateInfo;
+import com.example.weatherdiary.dto.ModifyDiaryRequestDto;
 import com.example.weatherdiary.service.DiaryService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -26,16 +28,16 @@ public class DiaryController {
     @PostMapping()
     public ResponseEntity createDiary(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-            @RequestBody Map<String, String> text // JSON 형식 그대로 들어오기 때문에 MAP 으로 받
+            @RequestBody CreateDiaryRequestDto request// JSON 형식 그대로 들어오기 때문에 MAP 으로 받기
     ) throws IOException {
-        diaryService.createDiary(date,text.get("text"));
+        diaryService.createDiary(date, request);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping()
     public ResponseEntity<List<Diary>> readDiary(
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
-    ){
+    ) {
         return new ResponseEntity<>(
                 diaryService.readDiary(date), HttpStatus.OK);
 
@@ -48,5 +50,14 @@ public class DiaryController {
         return new ResponseEntity<>(
                 diaryService.readDiaries(request), HttpStatus.OK
         );
+    }
+
+    @PutMapping("/{diaryId}")
+    public ResponseEntity modifiyDiary(
+            @RequestBody ModifyDiaryRequestDto request,
+            @PathVariable("diaryId") Long id
+    ) {
+        diaryService.modifyDiary(request, id);
+        return ResponseEntity.ok().build();
     }
 }
