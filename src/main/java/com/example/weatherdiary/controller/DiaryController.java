d67454b33699de01d8524cd8fa58cd01339597a5
choidongkuen <danaver12@daum.net>
2023-03-01 20:20:56 +0900

    feat : 일기 생성 컨트롤러 생성

--- src/main/java/com/example/weatherdiary/controller/DiaryController.java
+++ src/main/java/com/example/weatherdiary/controller/DiaryController.java
@@ -1,11 +1,30 @@
 package com.example.weatherdiary.controller;
 
+import com.example.weatherdiary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class DiaryController {
+
+    private final DiaryService diaryService;
+
+    @PostMapping()
+    public ResponseEntity createDiary(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @RequestBody String text
+    ) {
+        diaryService.createDiary(date,text);
+        return ResponseEntity.ok().build();
+    }
 }
