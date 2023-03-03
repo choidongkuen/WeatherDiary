827922232bbfd525a37a8821d4994037030f8e5c
choidongkuen <danaver12@daum.net>
2023-03-03 21:05:24 +0900

    feat : main 에 @EnableTransactionManagement 추가

--- src/main/java/com/example/weatherdiary/WeatherDiaryApplication.java
+++ src/main/java/com/example/weatherdiary/WeatherDiaryApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaAuditing
+@EnableTransactionManagement
 @SpringBootApplication
 public class WeatherDiaryApplication {
 
