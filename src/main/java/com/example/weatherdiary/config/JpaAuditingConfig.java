8b534282946ebaef57ed8eb91df8de00f6bb0f3a
choidongkuen <danaver12@daum.net>
2023-03-03 22:25:48 +0900

    fix : webMvcTest 단위 테스트시 JpaAuditing 오류 발생 수정
    JpaAuditingConfig 파일 따로 생성

---
+++ src/main/java/com/example/weatherdiary/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.example.weatherdiary.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
