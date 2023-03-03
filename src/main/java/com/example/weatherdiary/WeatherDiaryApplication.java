7160a2be447a5dfbe7c2cc3b53810ae8e0130834
choidongkuen <danaver12@daum.net>
2023-03-03 21:07:28 +0900

    feat : import 제거

--- src/main/java/com/example/weatherdiary/WeatherDiaryApplication.java
+++ src/main/java/com/example/weatherdiary/WeatherDiaryApplication.java
@@ -3,7 +3,6 @@ package com.example.weatherdiary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaAuditing
