fb76a6a61cfabb050a05b9df20d939b7a020e2c2
choidongkuen <danaver12@daum.net>
2023-03-06 08:51:28 +0900

    feat : DateWeatherRepository 메소드 추가

--- src/main/java/com/example/weatherdiary/domain/repository/DateWeatherRepository.java
+++ src/main/java/com/example/weatherdiary/domain/repository/DateWeatherRepository.java
@@ -4,7 +4,9 @@ import com.example.weatherdiary.domain.entity.DateWeather;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+    List<DateWeather> findAllByDate(LocalDate date);
 }
