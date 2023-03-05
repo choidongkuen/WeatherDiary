652b7ac6166ada17d157eecc9b406c245a50953c
choidongkuen <danaver12@daum.net>
2023-03-06 08:50:53 +0900

    feat : 날씨 데이터를 DB 저장하기 위해 DateWeather 엔티티 구현

---
+++ src/main/java/com/example/weatherdiary/domain/entity/DateWeather.java
@@ -0,0 +1,49 @@
+package com.example.weatherdiary.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Slf4j
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Where(clause = "deleted_at is null")
+@SQLDelete(sql = "UPDATE date_weather set date_weather.deleted_at = CURRENT_TIMESTAMP WHERE date_weather.id = ?")
+@Table
+@Entity
+public class DateWeather extends BaseEntity{
+
+    @Id
+    private LocalDate date;
+
+    @Column(name = "weather")
+    private String weather;
+
+    @Column(name = "icon")
+    private String icon;
+
+    @Column(name = "temperature")
+    private Double temperature;
+
+    public static DateWeather of(Map<String, Object> parsedWeather) {
+        return DateWeather.builder()
+                .weather(parsedWeather.get("main").toString())
+                .icon(parsedWeather.get("icon").toString())
+                .temperature((Double) parsedWeather.get("temp"))
+                .date(LocalDate.now())
+                .build();
+    }
+}
